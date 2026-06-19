9e6e899bdf4aa5032fc3bb7a36a0bf370743071a
Jose Luis Ramirez <jlra197634@gmail.com>
2026-06-19 16:50:59 -0600

    Corrige error por cambio en la interfaz Gif

--- src/mock-data/gifs.mock.ts
+++ src/mock-data/gifs.mock.ts
@@ -8,6 +8,7 @@ export const mockGifs: Gif[] = [
         url: 'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3ZiODVkcHNmeWR4eTVkcnY5OGZldmI0djlzMGd0MnoyZ3o0bHhtaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qRdGR2H9EtiXUJXorm/giphy.gif',
         width: 300,
         height: 300,
+        size: '1.8 mb',
     },
     {
         id: '2',
@@ -15,6 +16,7 @@ export const mockGifs: Gif[] = [
         url: 'https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif',
         width: 400,
         height: 250,
+        size: '1.8 mb',
     },
     {
         id: '3',
@@ -22,6 +24,7 @@ export const mockGifs: Gif[] = [
         url: 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcG40N2cyZm1iZGM2aWlhbHF1bnZmdzkxZ29idTY3ZzZkbnowNDg4OSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xatKdEypHxBjzabUPV/giphy.gif',
         width: 280,
         height: 350,
+        size: '1.8 mb',
     },
     {
         id: '4',
@@ -29,6 +32,7 @@ export const mockGifs: Gif[] = [
         url: 'https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif',
         width: 300,
         height: 200,
+        size: '1.8 mb',
     },
     {
         id: '5',
@@ -36,6 +40,7 @@ export const mockGifs: Gif[] = [
         url: 'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExaG5zMmVsNDFzam56bmR6azl0bmFvbHQxdDczeDNzZTdxczhtejNrNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fUQ4rhUZJYiQsas6WD/giphy.gif',
         width: 300,
         height: 400,
+        size: '1.8 mb',
     },
     {
         id: '6',
@@ -43,5 +48,6 @@ export const mockGifs: Gif[] = [
         url: 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNThyZXBwbHFtdHNpYnh4ZnN0aGswcWJydXNwdnpjaXV0eXNlYXp1YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8BSJinJ8of9L0unCqW/giphy.gif',
         width: 350,
         height: 280,
+        size: '1.8 mb',
     },
 ];
